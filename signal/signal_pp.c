821461ff4bb6057d90702f9d7abdbb2517136975
s5kywa1k3r <hestarium@hotmail.com>
2020-06-30 11:46:43 +0900

    clock_gettime 추가 - 컴파일 시 " -D CLOCK_GETTIME " 옵션 부여 시 사용 가능

--- signal/signal_pp.c
+++ signal/signal_pp.c
@@ -5,13 +5,23 @@
 #include <pthread.h>
 #include <sys/time.h>
 #include <sys/types.h>
+#include <time.h>
 
+#ifdef CLOCK_GETTIME
+struct timespec start_point, end_point;
+#else
 struct timeval start_point, end_point;
+#endif
+
 double measure = 0;
 pid_t pid1, pid2;
 
 void send_ping(){
+#ifdef CLOCK_GETTIME
+    clock_gettime(CLOCK_REALTIME, &start_point);
+#else
     gettimeofday(&start_point, NULL);
+#endif
     kill(pid2, SIGUSR1);
 }
 
@@ -20,7 +30,11 @@ void recv_ping(){
 }
 
 void recv_pong(){
+#ifdef CLOCK_GETTIME
+    clock_gettime(CLOCK_REALTIME, &end_point);
+#else
     gettimeofday(&end_point, NULL);
+#endif
 }
 
 int main(int argc, char *argv[]){
@@ -40,10 +54,13 @@ int main(int argc, char *argv[]){
         send_ping();
         pause();
     }
-
+#ifdef CLOCK_GETTIME
+    measure = (end_point.tv_sec - start_point.tv_sec) * 1000000000 + end_point.tv_nsec - start_point.tv_nsec;
+    measure /= 1000000;
+#else
     measure = (end_point.tv_sec - start_point.tv_sec) * 1000000 + end_point.tv_usec - start_point.tv_usec;
     measure /= 1000;
-
+#endif
     printf("[P] measure time: %f ms\n", measure);
 
     return 0;
