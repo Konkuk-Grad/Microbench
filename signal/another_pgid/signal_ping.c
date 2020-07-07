1cdb3c2abc2732663ac269804600c3d6b0f00280
s5kywa1k3r <hestarium@hotmail.com>
2020-07-07 11:01:59 +0900

    I don't know what i changed

--- signal/another_pgid/signal_ping.c
+++ signal/another_pgid/signal_ping.c
@@ -19,14 +19,17 @@ struct timeval *start_point, *end_point;
 
 struct msgbuf{
     long msgtype;
+    long measure_time;
     pid_t pid;
 };
 
-
+// Global Variables
 double measure = 0;
 int iter = 10, curr = 0; // Default Iteration is 10
 pid_t pid1, pid2;
 
+/* Signal handlers */
+// void sigcontHandler(){ return ; }
 void send_ping(){
 #ifdef CLOCK_GETTIME
     clock_gettime(CLOCK_REALTIME, &start_point[curr]);
@@ -36,15 +39,15 @@ void send_ping(){
     kill(pid2, SIGUSR1);
 }
 
-void recv_ping(){
-    kill(pid1, SIGUSR1); // SEND PONG
-}
-
 void recv_pong(){
 #ifdef CLOCK_GETTIME
     clock_gettime(CLOCK_REALTIME, &end_point[curr]);
 #else
-    gettimeofday(&end_point, NULL);
+    gettimeofday(&end_point[curr], NULL);
+#endif
+
+#ifdef PPDEBUG
+    printf("[DEBUG][pid: %d] (recv_pong) curr (%d)\n", pid1, curr);
 #endif
 }
 
@@ -54,7 +57,7 @@ int main(int argc, char *argv[]){
     struct msgbuf pingbuf;
 
     if(argc != 3){
-        printf("[Ping] argc was %d, argc must be 2!\n", argc);
+        printf("[Ping] argc was %d, argc must be 3!\n", argc);
         printf("[Ping] ./ping (Message Queue Key) (Iter)\n");
         exit(0);
     }
@@ -66,15 +69,17 @@ int main(int argc, char *argv[]){
 
     iter = atoi(argv[2]);
 
+    /* 1. Exchange pids between ping and pong */
     key_id = msgget((key_t)atoi(argv[1]), IPC_CREAT | 0666);
     if(key_id == -1){
-        perror("[Ping] msgget error: ");
+        perror("[Ping] ping-pong msgget error: ");
         exit(0);
     }
 
     printf("[Ping] Key: %d\n", key_id);
 
     pingbuf.msgtype = 1;
+    pingbuf.measure_time = 0;
     pingbuf.pid = getpid();
     
 #ifdef CLOCK_GETTIME
@@ -101,23 +106,81 @@ int main(int argc, char *argv[]){
     pid2 = pingbuf.pid; // Received Pong PID
     printf("[Ping] Ping PID: %d, Pong PID: %d\n", pid1, pid2);
 
-    signal(SIGUSR1, recv_pong); // Setting signal handler
+    /* 2. Send ping's pid to control process */
+    // key_id = msgget((key_t)111222333, IPC_CREAT | 0666);
+    // if(key_id == -1){
+    //     perror("[Ping] ctl msgget error: ");
+    //     exit(0);
+    // }
+
+    // pingbuf.msgtype = 10;
+    // pingbuf.pid = getpid();
+    // pingbuf.measure_time = 0;
+
+    // printf("[Ping] send to ctl\n");
+    // if(msgsnd(key_id, (void *)&pingbuf, sizeof(struct msgbuf), 0) == -1){
+    //     perror("[Ping] ctl msgsnd error: ");
+    //     exit(0);
+    // }
+
+    // printf("[Ping] send complete!\n");
+
+    /* 3. Setting handler and some settings */
+    signal(SIGUSR1, recv_pong); // Setting up signal handler
+    // signal(SIGCONT, sigcontHandler); // Setting up signal handler
+
+    /* 4. Wait SIGCONT from control process */
+    sleep(5); // Wait for creating pong process
+    // pause(); // Wait until SIGCONT from control process
+    printf("[Ping] (pid: %d) Get SIGCONT\n", pid1);
+
+    /* 5. Start Ping-Pong */
     for(curr = 0; curr < iter; curr++){
-        sleep(2);
+        // sleep(2); // For avoid race condition
         send_ping();
         pause();
-        
+    }
+
+    /* 6. Calculate measure time */
+    for(curr = 0; curr < iter; curr++){
 #ifdef CLOCK_GETTIME
-        measure += (double)((end_point[curr].tv_sec - start_point[curr].tv_sec) * 1000000000 + end_point[curr].tv_nsec - start_point[curr].tv_nsec) / 1000000;
+        // measure += (double)((end_point[curr].tv_sec - start_point[curr].tv_sec) * 1000000000 + end_point[curr].tv_nsec - start_point[curr].tv_nsec) / 1000000;
+        measure += ((end_point[curr].tv_sec - start_point[curr].tv_sec) * 1000 + (double)(end_point[curr].tv_nsec - start_point[curr].tv_nsec) / 1000000);
 #else
         measure += (double)((end_point[curr].tv_sec - start_point[curr].tv_sec) * 1000000 + end_point[curr].tv_usec - start_point[curr].tv_usec) / 1000;
 #endif
     }
 
-    measure /= iter;
+    measure /= iter; // Average
+
+    /* 7. Send to a control process */
+    // memset(&pingbuf, 0 ,sizeof(struct msgbuf));
+    // pingbuf.msgtype = 12;
+    // pingbuf.pid = pid1;
+    // pingbuf.measure_time = measure;
+    // if(msgsnd(key_id, (void *)&pingbuf, sizeof(struct msgbuf), 0) == -1){
+    //     perror("[Ping] ctl msgsnd error: ");
+    //     exit(0);
+    // }
+
+#ifdef PPDEBUG
+    printf("[DEBUG] PING pid (%d)\n", pid1);
+    for(int i = 0; i < iter; i++){
+#ifdef CLOCK_GETTIME
+        printf("[DEBUG] [pid: %d] [%d]: %f ms\n", pid1, i, (double)((end_point[i].tv_sec - start_point[i].tv_sec) * 1000000000 + end_point[i].tv_nsec - start_point[i].tv_nsec) / 1000000);
+#else
+        printf("[DEBUG] [pid: %d] [%d]: %f ms\n", pid1, i, (double)((end_point[i].tv_sec - start_point[i].tv_sec) * 1000000 + end_point[i].tv_usec - start_point[i].tv_usec) / 1000);
+#endif
+    }
+#endif
+
     printf("[Ping] measure time: %f ms\n", measure);
 
+    /* 7. Exit process */
     free(end_point);
     free(start_point);
+
+    sleep(2);
+    kill(pid2, SIGKILL); // Kill Pong Process
     return 0;
 }
