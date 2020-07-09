ac42f20d7d9addcd242088a26c9260c9aede7e10
s5kywa1k3r <hestarium@hotmail.com>
2020-07-09 23:23:03 +0900

    python3 script.py {Number of processes} {Number of Iterations}

--- signal/script.py
+++ signal/script.py
@@ -13,20 +13,30 @@ def exec_program(iter = 10):
 if __name__ == "__main__":
     # INPUT FROM USER
     pcnt = 1
+    noi = 10
     try :
         pcnt = int(sys.argv[1])
+        noi = int(sys.argv[2])
     except:
-        print("[Error] sys.argv[1] is not a positive integer. (Input: {0})".format(sys.argv[1]))
+        print("[Error] sys.argv[1] or sys.argv[2] is not a positive integer. (Input: {0}, {1})".format(sys.argv[1], sys.argv[2]))
     
+    context.log_level = 'error'
     for i in range(pcnt):
         # [Iteration count, Message Queue]
-        exec_program(10)
+        exec_program(noi)
 
     # WAIT UNTIL BEING RESULTED
     recv = []
     for i in range(pcnt):
-        print ("Wait: {0}".format(i))
-        recv.append(process_list[i].recvuntil("ms")[:-3])
+        # print ("Wait: {0}".format(i))
+        # process_list[i].recvuntil("[Average]")
+        process_list[i].recvuntil("{")
+        recv.append(float(process_list[i].recvuntil("}")[:-1]))
 
+    avg = 0
     for i in range(pcnt):
-        print("[{0}]: {1} ms".format(i, recv[i]))
+        # print("[{0}]: {1} ms".format(i, recv[i]))
+        avg += recv[i]
+    context.log_level = 'DEBUG'
+    # print("NoP: {0} / NoI: {1} / ART: {2} ms".format(pcnt, noi, avg / pcnt))
+    print(avg / pcnt)
