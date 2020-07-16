7cb440e02ec1912c9909bb305aab8e1b771b0f55
jjangsean <4066898@naver.com>
2020-07-16 00:39:11 -0700

    더미코드 제거 및 수정

--- semaphore/semaphore_test.c
+++ semaphore/semaphore_test.c
@@ -34,26 +34,6 @@ int consume_item() {
 	printf("C get %d\n", item);
 	return item;
 }
-/*
-int setSemaphore(int semID, int semOP) {
-	struct sembuf buf;
-
-	buf.sem_num = 0;
-	buf.sem_op = semOP;
-	buf.sem_flg = SEM_UNDO;
-
-	if (-1 == semop(semID, &buf, 1))
-		printf("%d faild\n", semID);
-}
-
-void wait(semaphore s) {
-	setSemaphore(s, -1);
-}
-
-void signal(semaphore s) {
-	setSemaphore(s, 1);
-}
-*/
 
 void* producer(void* arg) {
 	int item;
@@ -102,48 +82,12 @@ int main(int argc, char *argv[]) {
 	pthread_t thread1;
 	pthread_t thread2;
 	double time;
-	const long long NANOS = 1000000000LL;
-	/*union semun {
-		int	val;
-		struct semid_ds* buf;
-		unsigned short int* array;
-	}arg;
-	*/
+
 	N = atoi(argv[1]);
 	if(isdigit(N)!=0){
-		printf("Error!\n");
-		return 0;
-	}
-		
-	/*
-	if ((Empty = semget(IPC_PRIVATE, 1, IPC_CREAT | 0666)) == -1) {
-		printf("semget_Empty Error\n");
+		printf("Input NUM Error!\n");
 		return -1;
 	}
-	arg.val = MAX;
-	if (semctl(Empty, 0, SETVAL, arg) == -1) {
-		printf("semctl_Empty Error\n");
-		return -1;
-	}
-	if ((Mutex = semget(IPC_PRIVATE, 1, IPC_CREAT | 0666)) == -1) {
-		printf("semget_Mutex Error\n");
-		return -1;
-	}
-	arg.val = 1;
-	if (semctl(Mutex, 0, SETVAL, arg) == -1) {
-		printf("semctl_Mutex Error\n");
-		return -1;
-	}
-	if ((Full = semget(IPC_PRIVATE, 1, IPC_CREAT | 0666)) == -1) {
-		printf("semget_Full Error\n");
-		return -1;
-	}
-	arg.val = 0;
-	if (semctl(Full, 0, SETVAL, arg) == -1) {
-		printf("semctl_Full Error\n");
-		return -1;
-	}
-	*/
 
 	if((sem_init(&Empty,0,MAX))!=0)
 	{
@@ -166,26 +110,10 @@ int main(int argc, char *argv[]) {
 	pthread_join(thread1, NULL);
 	pthread_join(thread2, NULL);
 
-/*
-	if(semctl(Empty,0,IPC_RMID,arg)){
-		printf("semctl_Empty_RM Error\n");
-		return -1;
-	}
-	if(semctl(Mutex,0,IPC_RMID,arg)){
-		printf("semctl_Mutex_RM Error\n");
-		return -1;
-	}
-	if(semctl(Full,0,IPC_RMID,arg)){
-		printf("semctl_Full_RM Error\n");
-		return -1;
-	}
-	*/
-
-	//sem_destroy(&Empty);
-	//sem_destroy(&Full);
-	//sem_destroy(&Mutex);
+	sem_destroy(&Empty);
+	sem_destroy(&Full);
+	sem_destroy(&Mutex);
 	time = (end.tv_sec-begin.tv_sec) * 1000 + (double)(end.tv_nsec-begin.tv_nsec) / 1000000;
-	// time = (double)time/1000000;
 
 	printf("time : %f\n", time/N);
 	return 0;
