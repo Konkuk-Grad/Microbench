873e00a05a0cdb1fe18e9e7b8ee194096dcb6442
s5kywa1k3r <hestarium@hotmail.com>
2020-07-20 22:39:22 +0900

    함수로 변경

--- signal/script.py
+++ signal/script.py
@@ -6,43 +6,119 @@ exec_file_name = "./signal_pp.out"
 
 process_list = []
 
+max_cores = 0
 def exec_program(iter = 10):
     p = process([exec_file_name] + [str(iter)])
     process_list.append(p)
     p.recvuntil("{")
-    return int(p.recvuntil(",")[:-1])
+    g = [int(p.recvuntil(",")[:-1]), int(p.recvuntil("}")[:-1])]
+    return g
 
-if __name__ == "__main__":
-    # INPUT FROM USER
-    pcnt = 1
-    noi = 10
-    try :
-        pcnt = int(sys.argv[1])
-        noi = int(sys.argv[2])
-    except:
-        print("[Error] sys.argv[1] or sys.argv[2] is not a positive integer. (Input: {0}, {1})".format(sys.argv[1], sys.argv[2]))
-    
+def init_menu():
+    ip = -1
+    while ip < 0 or ip >= 3:
+
+        print("Select a testing type")
+        print("1. Signal")
+        print("2. IPC")
+        print("3. Lock")
+        print("0. exit")
+
+        ip = int(input())
+
+    if ip == 1: # 1. Signal
+        sigtest()
+        return 1
+    elif ip == 2:
+        return 2
+    elif ip == 3:
+        return 3
+    elif ip == 0:
+        print("Terminate program")
+        sys.exit(0)
+    else:
+        print("Invalid Input ({0})".format(ip))
+        sys.exit(-1)
+
+
+
+''' Signal '''
+# Initial execution function
+def sigtest():
+    print("1. Signal")
     context.log_level = 'error'
+    
+    test_attr = sigtestsetting()
+    sigtestiterexec(test_attr)
+
+    context.log_level = 'DEBUG'
+
+    
+
+
+
+# Set attribute
+def sigtestsetting():
+    pcnt, ni, nc, nt, ng = [0,0,0,0,0]
+    while pcnt <= 0 or ni <= 0 or nc <= 0 or nt <= 0:
+        if pcnt <= 0:
+            pcnt = int(input("Number of processes: "))
+        if ni <= 0:
+            ni = int(input("Number of iterations: "))
+        if nc <= 0:
+            nc = int(input("Number of cores: "))
+            nc = -1 if nc > max_cores else nc
+        if nt <= 0:
+            nt = int(input("Number of tests:"))
+        if ng <= 0:
+            ng = int(input("Variations of graphs: "))
+
+    return [pcnt, ni, nc, nt, ng]
+
+    
+def sigtestinit(test_attr):
+    pcnt, ni, nc, nt, ng = test_attr
+
     pid_list = []
+    affinity_mask = {i for i in range(0, nc)}
+
+    process_list.clear()
     for i in range(pcnt):
-        # [Iteration count, Message Queue]
-        pid_list.append(exec_program(noi))
+        pid_list.append(exec_program(ni))
 
     for pid in pid_list:
-        os.kill(pid, signal.SIGCONT)
-    
-    # WAIT UNTIL BEING RESULTED
-    recv = []
-    for i in range(pcnt):
-        # print ("Wait: {0}".format(i))
-        # process_list[i].recvuntil("[Average]")
-        process_list[i].recvuntil("{")
-        recv.append(float(process_list[i].recvuntil("}")[:-1]))
+        print(pid)
+        os.sched_setaffinity(pid[0], affinity_mask)
+        os.sched_setaffinity(pid[1], affinity_mask)
 
-    avg = 0
-    for i in range(pcnt):
-        # print("[{0}]: {1} ms".format(i, recv[i]))
-        avg += recv[i]
-    context.log_level = 'DEBUG'
-    # print("NoP: {0} / NoI: {1} / ART: {2} ms".format(pcnt, noi, avg / pcnt))
-    print(avg / pcnt)
+    return pid_list
+
+
+def sigtestexec(pid_list):
+    for pid in pid_list:
+        os.kill(pid[0], signal.SIGCONT)
+
+    recv = []
+    for p in process_list:
+        p.recvuntil("{")
+        recv.append(float(p.recvuntil("}")[:-1]))
+    avg = sum(recv) / len(pid_list)
+    return avg
+
+def sigtestiterexec(test_attr):
+    result = {}
+    for i in range(test_attr[3]):
+        pid_list = sigtestinit(test_attr)
+        result[i] = sigtestexec(pid_list)
+
+    for key in result:
+        print ("{0}: {1}".format(key, result[key]))
+
+    return result
+
+
+# Program starts
+if __name__ == "__main__":
+    max_cores = os.cpu_count()
+    init_menu()
+    
