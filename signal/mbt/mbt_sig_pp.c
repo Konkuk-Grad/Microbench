fbeec864db5021c0d9a88109c529ab5c1c0a9b46
s5kywa1k3r <hestarium@hotmail.com>
2020-07-29 16:39:02 +0900

    Move files to mbt_signal folder

--- signal/mbt/mbt_sig_pp.c
+++
@@ -1,5 +0,0 @@
-#include "mbt_sig_pp.h"
-
-int sub(int a, int b){
-    return a - b;
-}
--- signal/mbt/mbt_signal.c
+++
@@ -1,6 +0,0 @@
-#include "mbt_signal.h"
-
-int add(int a, int b){
-    printf("sub2(%d, %d): %d\n", a, b, sub(a, b));
-    return a + b;
-}
