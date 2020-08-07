3b50c607b652c403e60e3c2a4dc8c5d47c961da9
jjangsean <4066898@naver.com>
2020-08-06 23:19:17 -0700

    mbti통합용 semaphore 헤더와 c파일 생성

---
+++ mbti/mbti_semaphore/mbti_sem.h
@@ -0,0 +1,36 @@
+/***** Parent Header *****/
+#include "../mbti.h"
+
+// Start mbti_signal.h
+
+/* ------- */
+/* Headers */
+/* ------- */
+#include <pthread.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <time.h>
+#include <semaphore.h>
+#include <ctype.h>
+
+/* ---------------- */
+/* Global Variables */
+/* ---------------- */
+static sem_t sem_empty, sem_full, sem_mutex;
+//버퍼가 비었는지, 찼는지, 버퍼에 접근 가능한지에 대한 semaphore변수
+int sem_buffer[1], sem_front, sem_rear;
+//scsp 상황에서 공유되는 버퍼, 버퍼의 앞 과 뒤를 가르키는 변수들
+int sem_count,sem_user_iter,sem_user_processes;
+//현재 반복한 횟수, 유저가 입력한 반복 횟수, 유저가 입력한 프로세스 개수
+struct timespec sem_begin, sem_end;
+//시간 표시를 위한 시작 시각, 마치는 시각
+
+/* --------- */
+/* Functions */
+/* --------- */
+int sem_produce_item();
+int sem_put_item(int item);
+int sem_consume_item();
+void* sem_producer(void* arg);
+void* sem_consumer(void* arg);
+double sem_test_exec(int topology, int processes, int iter, int num_cpus);
