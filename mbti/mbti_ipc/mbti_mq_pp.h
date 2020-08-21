140156b1b09a4f141ce6a5197daf6728469abf44
Sungjun Park <parks8109@naver.com>
2020-08-21 23:47:48 +0900

    Debug error increasing process number
    MBTI IPC work out 3 problem.
    1. By using message queue, it can measure average of mbti time.
    2. By using kernel shark, make sure core affinity working.
    3. By changing system setting, it can increase processes number enough.

--- mbti/mbti_ipc/mbti_mq_pp.h
+++ mbti/mbti_ipc/mbti_mq_pp.h
@@ -10,27 +10,40 @@
 /* Defines */
 /* ------- */
 
+#define MQ_PP_NAME "/mq_pp"
 #define MQ_MAX_MSG 10 // Count
 #define MQ_MSG_SIZE 4 // Byte
+#define MQ_PP_MAX_MSG 1025 // Count
+#define MQ_PP_MSG_SIZE 8 // Byte
+#define SHM_KEY_NUM 9527
+#define SHM_MEM_SIZE 1024
 
 /* ------- */
 /* Headers */
 /* ------- */
 
 #include <sched.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 
 /* ---------------- */
 /* Global Variables */
 /* ---------------- */
 
+int mq_pp_shmid;
+
 /* --------- */
 /* Functions */
 /* --------- */
 
-int mq_pingpong(int pairs, int iter, int cores);
-int mq_ping(int num, int iter);
-int mq_pong(int num, int iter);
+double mq_pingpong(int pairs, int iter, int cores);
+double mq_ping(int num, int iter, int cores);
+int mq_pong(int num, int iter, int cores);
 void set_core_affinity(int cores);
-long long get_time_diff(unsigned int flag);
+double get_time_diff(unsigned int flag);
+void shm_create();
+void shm_write(long long *shareddata, int size);
+void shm_read(long long *sMemory);
+void shm_free();
 
 #endif
