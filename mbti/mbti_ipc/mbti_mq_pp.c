b70e24e4d794cabbba541803571f2e3b00edbe1b
Sungjun Park <parks8109@naver.com>
2020-10-20 21:02:41 +0900

    Debug mq_unlink argument and update mqueue data size to 1byte
    mq_unlink argument is changed.
    mqueue data size changed 4byte to 1byte.
    It has issue when mqueue data size was 0byte.

--- mbti/mbti_ipc/mbti_mq_pp.c
+++ mbti/mbti_ipc/mbti_mq_pp.c
@@ -51,7 +51,7 @@ double mq_ping(int num, int iter, int cores){
     	mqd_t ping_mqdes, pong_mqdes; // 메세지 큐 디스크립터 타입
     	unsigned int ping_prio = 10, pong_prio;
 	double time = 0, measure_time = 0;
-	int value;
+	char value = 0;
 	char ping_name[30] = "/mq_ping";
 	char pong_name[30] = "/mq_pong";
 	char ping_num[15] = { 0 };
@@ -75,7 +75,7 @@ double mq_ping(int num, int iter, int cores){
 
 	for(int i = 0; i < iter; i++) {
         	//printf("%d ping sending a message %d with priority %d\n", num, i, ping_prio);
-        	if(mq_send(pong_mqdes, (char *)&i, MQ_MSG_SIZE, ping_prio) == -1) {
+        	if(mq_send(pong_mqdes, (char *)&value, MQ_MSG_SIZE, ping_prio) == -1) {
             		perror("mq_send() fail\n");
 		}
 		
@@ -91,7 +91,7 @@ double mq_ping(int num, int iter, int cores){
     	//printf("Pair%d PingPong Elapsed Time: %f \n", num/2, time);
     	
 	mq_close(ping_mqdes);
-    	mq_unlink(strcat(ping_name, ping_num));
+    	mq_unlink(ping_name);
 
     	return time;
 }
@@ -101,7 +101,7 @@ int mq_pong(int num, int iter, int cores){
         struct mq_attr attr; // 메세지 큐 특성 설정 구조체
         mqd_t ping_mqdes, pong_mqdes; // 메세지 큐 디스크립터 타입
         unsigned int ping_prio, pong_prio = 20;
-	int value;
+	char value = 0;
 	char ping_name[30] = "/mq_ping";
 	char pong_name[30] = "/mq_pong";
 	char ping_num[15] = { 0 };
@@ -127,13 +127,13 @@ int mq_pong(int num, int iter, int cores){
                         //printf("%d pong received a message %d with priority %d\n", num, value, ping_prio);
                 }
                 //printf("%d pong sending a message %d with priority %d\n", num, i, pong_prio);
-                if(mq_send(ping_mqdes, (char *)&i, MQ_MSG_SIZE, pong_prio) == -1) {
+                if(mq_send(ping_mqdes, (char *)&value, MQ_MSG_SIZE, pong_prio) == -1) {
                         perror("mq_send() fail\n");
                 }
         }
 
         mq_close(pong_mqdes);
-        mq_unlink(strcat(pong_name, pong_num));
+        mq_unlink(pong_name);
 
         return 0;
 }
