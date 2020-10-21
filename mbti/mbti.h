67003ee033fe424e3df56b4b5600895550c29ef0
s5kywa1k3r <hestarium@konkuk.ac.kr>
2020-10-21 10:59:23 +0900

    Make defines for printing debug messages

--- mbti/mbti.h
+++ mbti/mbti.h
@@ -10,6 +10,36 @@
 #include <unistd.h>
 #include <string.h>
 
+/**** Defines ****/
+/** Print type **/
+// Colors
+#define ANSI_COLOR_RED "\x1b[31m" 
+#define ANSI_COLOR_GREEN "\x1b[32m" 
+#define ANSI_COLOR_YELLOW "\x1b[33m" 
+#define ANSI_COLOR_BLUE "\x1b[34m" 
+#define ANSI_COLOR_MAGENTA "\x1b[35m" 
+#define ANSI_COLOR_CYAN "\x1b[36m" 
+#define ANSI_COLOR_RESET "\x1b[0m" 
+
+#ifdef __DEBUGMSG
+#define DEBUGMSG(fmt, args...) fprintf(stderr, ANSI_COLOR_YELLOW"[DEBUGMSG]"ANSI_COLOR_CYAN"{%s:%d:%s()}: "ANSI_COLOR_RESET fmt, \
+__FILE__, __LINE__, __func__, ##args)
+#define PRINTWARN(fmt, args...) fprintf(stdout, ANSI_COLOR_YELLOW"[WARN]"ANSI_COLOR_CYAN"{%s:%d:%s()}: "ANSI_COLOR_RESET fmt, \
+__FILE__, __LINE__, __func__, ##args)
+#define PRINTLOG(fmt, args...) fprintf(stdout, ANSI_COLOR_BLUE"[LOG]"ANSI_COLOR_CYAN"{%s:%d:%s()}: "ANSI_COLOR_RESET fmt, \
+__FILE__, __LINE__, __func__, ##args)
+#else
+#define DEBUGMSG(fmt, args...)
+#define PRINTWARN(fmt, args...)
+#define PRINTLOG(fmt, args...)
+#endif
+
+#define PRINTERROR(fmt, args...) fprintf(stderr, ANSI_COLOR_RED"[ERROR]"ANSI_COLOR_CYAN"{%s:%d:%s()}: "ANSI_COLOR_RESET fmt, \
+__FILE__, __LINE__, __func__, ##args)
+
+#define PRINTRESULT(fmt, args...) fprintf(stdout, ANSI_COLOR_GREEN"[RESULT]"ANSI_COLOR_CYAN"{%s:%d:%s()}: "ANSI_COLOR_RESET fmt, \
+__FILE__, __LINE__, __func__, ##args)
+
 #endif
 
 // Start mbti.h
